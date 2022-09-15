0dc465d1c0e6c5096801b2899129b193a805c3b8
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-09-15 10:22:26 -0400

    fixed output of the function

--- main.cpp
+++ main.cpp
@@ -19,11 +19,8 @@ int main()
 	std::cout << "\nPlease enter a date: 05/20/2018";
 
 	std::cout << "\n\n";
-	get_east_storage("05/20/2018");
+	std::cout << "East basin storage: " << get_east_storage("05/20/2018") << "  billion gallons";
 	std::cout << std::endl;
 	
 	return 0;
-
-  
-  return 0;
 }
