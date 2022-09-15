fd83f8423d8896f4e844a7827106f5a8339939ee
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-09-15 10:07:31 -0400

    added header file with get_east_storage() included

--- reservoir.h
+++ reservoir.h
@@ -1,2 +1,13 @@
-// add your code
+/*
+        Author: Halil Akca
+        Course: CSCI-135
+        Instructor: Mike Zamansky
+        Assignment: Lab 03, Task A
+        This file serves as a means to let one file use the contents of another file.
+*/
+#include <iostream>
+#include <string>
+#pragma once
+
+double get_east_storage(std::string date);
 
