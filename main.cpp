5dd121c40ee817b0d191b4e0b446095dbe1e2f78
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-09-15 10:09:12 -0400

    made progress on calling function from reservoir.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
-// add your code as appropriate 
+/*
+        Author: Halil Akca
+        Course: CSCI-135
+        Instructor: Mike Zamansky
+        Assignment: Lab 02, Task A
+
+        This program will call the function made in reservoir.cpp.
+*/
+
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <climits>
+#include <cstring>
+#include "reservoir.h"
 
 int main()
 {
-  
+	std::cout << "\nPlease enter a date: 05/20/2018";
+
+	std::cout << "\n\n";
+	get_east_storage("05/20/2018");
+	std::cout << std::endl;
+	
+	return 0;
 
   
   return 0;
