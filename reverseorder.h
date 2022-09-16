0b2289e66e0022e17586b34c887986a70527746e
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-09-15 21:18:24 -0400

    added the reverse_order() function

---
+++ reverseorder.h
@@ -0,0 +1,12 @@
+/*
+        Author: Halil Akca
+        Course: CSCI-135
+        Instructor: Mike Zamansky
+        Assignment: Lab 03, Task D
+        This file serves as a means to let one file use the contents of another file.
+*/
+#include <iostream>
+#include <string>
+#pragma once
+
+void reverse_order(std::string date1, std::string date2);
